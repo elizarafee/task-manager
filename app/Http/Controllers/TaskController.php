d883cf85f4766bd3d59385a34b1bf2067a3e47f8
Eliza Ahmed <elizarafee@gmail.com>
2020-01-03 19:59:25 +0000

    task controller preset done

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index() {
+        echo "index";
+    }
+
+    public function create() {
+        echo "create";
+    }
+
+    public function store(Request $request) {
+        echo "<pre>";
+        print_r($request->all());
+        echo "</pre>";
+    }
+
+    public function edit($task_id) {
+        echo 'edit '.$task_id;
+    }
+
+    public function update(Request $request, $task_id) {
+        echo 'put '.$task_id;
+        echo "<pre>";
+        print_r($request->all());
+        echo "</pre>";
+
+    }
+
+    public function completed($task_id) {
+        echo 'complete '.$task_id;
+    }
+
+    public function destroy($task_id) {
+        echo 'delete '.$task_id;
+    }
+}
